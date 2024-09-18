71d943138f0f1358040cae0a3e586a7335307925
Rcoder-singh <rishabh22.singh@gmail.com>
2024-09-18 10:28:04 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from "./Components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
